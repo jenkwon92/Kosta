5f48de1c0f8275e773814a5007eff25561710d83
Jen Kwon <70299766+jenkwon92@users.noreply.github.com>
2021-08-11 08:26:52 +0900

    210810

--- 09-Polymorphism-Object-Casting/src/step8/solution/CompanyService.java
+++ 09-Polymorphism-Object-Casting/src/step8/solution/CompanyService.java
@@ -3,44 +3,55 @@ package step8.solution;
 import step5.Employee;
 
 public class CompanyService {
-	//부모 Employee 타입의 배열로 선언해서 Employee 및 자식 객체들(Manager, Engineer) 을 저장하게 된다
+	// 부모 Employee 타입의 배열로 선언해서 Employee 및 자식 객체들(Manager, Engineer) 을 저장하게 된다
 	private Employee[] array;
-	//현재 배열에 저장된 총 사원수
-	private int count; //instance variable은 선언만 해도 기본 초기화0
+	// 현재 배열에 저장된 총 사원수
+	private int count; // instance variable은 선언만 해도 기본 초기화0
+
 	public CompanyService(int length) {
-		array = new Employee[length]; //전달받은 인자값 10의 길이의 배열을 생성한다. (사원, 객체들을 저장)
+		array = new Employee[length]; // 전달받은 인자값 10의 길이의 배열을 생성한다. (사원, 객체들을 저장)
 	}
+
 	public void add(Employee e) {
-		array[count++]=e; //array의 count index 공간에 요소를 추가한 후 count는 1이 증가된다
+		array[count++] = e; // array의 count index 공간에 요소를 추가한 후 count는 1이 증가된다
 	}
-	
+
 	public void printAll() {
-		for(int i=0; i<count;i++) {
-			System.out.println(array[i]); //println 내부적으로 toString()이 호출되어 출력된다.
+		for (int i = 0; i < count; i++) {
+			System.out.println(array[i]); // println 내부적으로 toString()이 호출되어 출력된다.
 		}
 	}
+
 	public Employee findEmployeeByEmpNo(String empNo) {
-		Employee employee=null;
-		for(int i=0; i<count; i++) {
-			if(array[i].getEmpNo().equals(empNo)) {
+		Employee employee = null;
+		for (int i = 0; i < count; i++) {
+			if (array[i].getEmpNo().equals(empNo)) {
 				employee = array[i];
 				break;
 			}
 		}
 		return employee;
 	}
-	
-	/*
-	public Employee[] findEmployeeByEmpNo(String empNo) {
-		for(int i=0; i<array.length; i++) {
-			if(array[i].getEmpNo().equals(empNo)) {
-				return array[i];
-			} 
+
+	public void deleteEmployeeByEmpNo(String empNo) {
+		Employee arr = null;
+		for (int i = 0; i < count; i++) {
+			if (empNo.equals(array[i].getEmpNo())) {
+				array[i] = arr;
+				array[i] = array[i + 1];
+				break;
+			} else {
+				System.out.println(empNo + " 사원번호에 해당하는 사원이 없어 삭제 불가");
+				break;
+			}
+
 		}
-		return array;
 	}
-	*/
-	
-	
-	
+
+	/*
+	 * public Employee[] findEmployeeByEmpNo(String empNo) { for(int i=0;
+	 * i<array.length; i++) { if(array[i].getEmpNo().equals(empNo)) { return
+	 * array[i]; } } return array; }
+	 */
+
 }
