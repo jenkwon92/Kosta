ac87d57e97f6410e03ba9ac2cfc37933c75e6f45
Jen Kwon <70299766+jenkwon92@users.noreply.github.com>
2021-08-19 18:37:45 +0900

    210819

--- 16-String-StringBuilder-IO/src/step7/TestOutput1.java
+++ 16-String-StringBuilder-IO/src/step7/TestOutput1.java
@@ -11,14 +11,15 @@ import java.io.PrintWriter;
  */
 public class TestOutput1 {
 	public static void main(String[] args) {
-		//장치에 연결되는 Node Stream 계열을 생성
 		try {
+			//장치에 연결되는 Node Stream 계열을 생성
 			FileWriter fw = new FileWriter("C:\\kosta224\\iotest\\memo.txt");
 			//기능을 지원하는 Processing Stream 계열을 생성
 			PrintWriter pw = new PrintWriter(fw);
 			pw.print("쉬는시간입니다");
 			System.out.println("파일 생성 및 출력");
-			pw.close();
+			pw.close(); //스트림을 닫아준다(이떄 추가적으로 메모리(buffer)에 있는 데이터를 실제 파일에 출력한다 - flush() 호출)
+			//스트림을 close 즉, 닫아준다는 것은 장치에 연결된 정보를 해제함으로써 다른 작업자가 해당 device를 제어할 수 있도록 한다는 의미
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
